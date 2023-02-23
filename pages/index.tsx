be0ba063fecccaa4e5dc1c3572bc6c268f38adb0
Nitin Pradeep Kumar <getnpk@gmail.com>
2023-02-23 19:56:32 +0900

    Set up tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
     return (
-        <div>
+        <h1 className="text-3xl font-bold underline">
             THB
-        </div>
+        </h1>
     )
 }
 
